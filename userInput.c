18008bef85ddb63b6eea4fefb289b73710634010
Damian Sikora <damiansikora256@gmail.com>
2023-10-02 11:11:33 +0100

    header updated

--- userInput.c
+++ userInput.c
@@ -1,8 +1,13 @@
-/***********************************************************************************
+/******************************************************************************
 *
-*  File Name: userInput.c
+* File Name: userInput.c
 *
+* Description: Function that will add and print the two given variable a and b
 *
+* Programmer: Damian Sikora
 *
+* Date: 02/Oct/2023
 *
-*/
+* Version 0.1
+*
+******************************************************************************/
