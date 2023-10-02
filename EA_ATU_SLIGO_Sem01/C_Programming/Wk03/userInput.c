9cd4e9e6ad551be3c73e9f1b1502135f94188718
Damian Sikora <damiansikora256@gmail.com>
2023-10-02 12:41:48 +0100

    userInput updated and Version changed from0.1to1.0

--- EA_ATU_SLIGO_Sem01/C_Programming/Wk03/userInput.c
+++ EA_ATU_SLIGO_Sem01/C_Programming/Wk03/userInput.c
@@ -8,14 +8,33 @@
 *
 * Date: 02/Oct/2023
 *
-* Version 0.1
+* Version 1.0
 *
 ******************************************************************************/
+#include <stdio.h>
 
+void main(vois){
+    int x = 0;
+    printf("Please enter the integer numcer: ");
+    scanf("%d",&x);/*important note on this line is the sign &*/
+    printf("\nThe number you have been entered = %d\n", x);
 
+    system("pause");
+    return 0;
+}
 
 /******************************************************************************
 *
 * Task:
+*     Use the scanf function to take in an integer and then print this number
+*     to the screen. 
+*     e.g.
+*
+*     int x;
+*
+*     scanf("%d", &x);
+*
+*     The & before the variable is very important and is the most common
+*     mistake with the scanf function to forget it.
 *
 ******************************************************************************/
