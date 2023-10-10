e82d6f21b53ca173822d56337a2eed02dfe0d09a
Damian Sikora <damiansikora256@gmail.com>
2023-10-10 11:03:57 +0100

    initial release of notEqual

---
+++ EA_ATU_SLIGO_Sem01/C_Programming/Wk04/notEqual.c
@@ -0,0 +1,41 @@
+/******************************************************************************
+*
+* File Name: notEqual.c
+*
+* Description: Program to print info on screen when user input not equal 5.
+*
+* Programmer: Damian Sikora
+*
+* Date: 10/Oct/2023
+*
+* Version 1.0
+*
+******************************************************************************/
+#include <stdio.h>
+
+int main(void){
+
+    int usersNumber = 0;
+
+    printf("Enter the integer number: ");
+    scanf("%d", &usersNumber);
+
+    if (!(usersNumber==5)){
+        printf("The number you input is Not equal to 5.\n");
+    }
+
+    system("pause");
+    return 0;
+}
+
+/******************************************************************************
+*
+* Task:
+*    
+*    Write a program that takes in a number from the user and if the number is
+*    Not equal to 5, prints out on the screen,
+*
+*    The number you input is Not equal to 5.
+*
+*
+******************************************************************************/
