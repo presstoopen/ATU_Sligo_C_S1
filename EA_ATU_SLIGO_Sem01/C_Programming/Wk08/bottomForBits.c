eb7f43fb33d43185d6d7339fd574070153a31559
Damian Sikora <damiansikora256@gmail.com>
2023-11-24 15:24:13 +0000

    last four bits  final

--- EA_ATU_SLIGO_Sem01/C_Programming/Wk08/bottomForBits.c
+++ EA_ATU_SLIGO_Sem01/C_Programming/Wk08/bottomForBits.c
@@ -2,11 +2,12 @@
 *
 * File Name: bottomForBits.c
 *
-* Description: Check if LSB on the user input is 0 or 1.
+* Description: Program to determine what is representing bottom 4 bits in
+               number given by user.
 *
 * Programmer: Damian Sikora
 *
-* Date: 14/Nov/2023
+* Date: 24/Nov/2023
 *
 * Version 1.0
 *
@@ -19,10 +20,10 @@ int main(void){
     int userInput = 0;
     int bottomFourth = 0;
 
-    printf("Enter first integer number: ");
+    printf("Enter number: ");
     scanf("%d", &userInput);
 
-    bottomFourth = userInput & 0x8;
+    bottomFourth = userInput & 0x0F;
   
     printf("The bottom fourth bit of %d = %d\n", userInput, bottomFourth);
 
