6103d2ac3509d42e10ddf6a00a7a7f43c899963a
Damian Sikora <damiansikora256@gmail.com>
2023-12-06 15:01:39 +0000

    numerals to text initial

---
+++ EA_ATU_SLIGO_Sem01/C_Programming/Wk09/numeralsToText.c
@@ -0,0 +1,61 @@
+/******************************************************************************
+*
+* File Name: numeralsToText.c
+*
+* Description: Program teaks numerical input form the user and convert it to
+               text format.
+*
+* Programmer: Damian Sikora
+*
+* Date: 6/Dec/2023
+*
+* Version 1.0
+*
+******************************************************************************/
+#include <stdio.h>
+
+char toUpperCase(char userInput);
+
+int main(void){
+
+    int userInput;
+    char *decodedText[] = {""};
+    int i = 0;
+    int modNumber;
+    char *numAsText[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    printf("Enter the number: ");
+    scanf("%d", &userInput);
+    
+    while (userInput>0){
+    	
+        modNumber = userInput%10;
+        decodedText[i]=numAsText[modNumber];
+        //printf("%s ", numAsText[modNumber]);
+        userInput = userInput / 10;
+        i++;
+    }
+    
+    while(i>0){
+    	printf("%s ", decodedText[i]);
+    	i--;
+	}
+    
+ 
+    system("pause");
+    return 0;
+}
+
+/******************************************************************************
+*
+* Task:
+*    
+*    Write a program that converts numbers to words. For example, 895 results
+*    in eight nine five.
+*
+*    The number should be taken in from the user when the programme is run.
+*
+*    The text should then be printed out on the screen. 
+*
+*
+******************************************************************************/
